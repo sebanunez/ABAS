2b494c2fad324656dfe904fb7ab495f6e63b21ab
Sebastian Nuñez <snunez@outlook.com.ar>
2021-12-27 22:23:22 -0300

    Estado inicial de pag inicio y estilos

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Administración de Beneficiarios del Área Social (ABAS) - BALP</title>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+Administración de Beneficiarios del Área Social (ABAS)
+<!-- LO QUE BUSCO ES HACER Y DESHACER -->
+</body>
+</html>
