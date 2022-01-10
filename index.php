4e22ac4c4418711b7869508f886ffabfb01df3f8
sebanunez <snunez@outlook.com.ar>
2022-01-10 00:03:45 -0300

    Se aplicó Boostrap básico

--- index.php
+++ index.php
@@ -1,13 +1,50 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Administración de Beneficiarios del Área Social (ABAS) - BALP</title>
-	<link rel="stylesheet" href="estilos.css">
-</head>
-<body>
-Administración de Beneficiarios del Área Social (ABAS)
-<!-- LO QUE BUSCO ES HACER Y DESHACER -->
-</body>
+<!doctype html>
+<html lang="es">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Administración de Beneficiarios del Área Social (ABAS) - BALP</title>
+  </head>
+  <body>
+    
+  	<div class="container" style="text-align: center;">
+  	<h1>Administración de Beneficiarios del Área Social (ABAS)</h1>	
+  
+  	<div class="row" style="justify-content: center; align-items: center;">
+  	
+  	<div class="col-4">
+  		Bienvenido a ABAS, el sistema de Administración de Beneficiarios del Área Social.
+  	</div>
+
+  	<div class="col-4">  	
+  	<form>  
+  	
+  	<div class="mb-2">
+    <label for="exampleInputEmail1" class="form-label">Usuario</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" required>
+	</div>
+  
+	<div class="mb-2">
+    <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" required>
+  	</div>
+ 	
+  	<button type="submit" class="btn btn-primary">Iniciar sesión</button>
+
+	</form>
+	</div>
+
+	</div>
+  	</div>
+    
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+  </body>
 </html>
