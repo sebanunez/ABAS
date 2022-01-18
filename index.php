b9fe8a2a350180a731681ae5793539dff512e862
NUNEZ SEBASTIAN <snunez@cgp.gba.gov.ar>
2022-01-18 10:04:20 -0300

    Cambio de aspecto del cuadro login

--- index.php
+++ index.php
@@ -9,50 +9,70 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <title>Administración de Beneficiarios del Área Social (ABAS) - BALP</title>
-  </head>
-  <body style="background-color: lightsteelblue;">
-    
-  	<div class="container-fluid" style="text-align: center; background: linear-gradient(to bottom, cornflowerblue, lightsteelblue);">
 
+</head>
+
+
+<body style="background-color: lightsteelblue;">
+    
+  <div class="container-fluid" style="text-align: center; background: linear-gradient(to bottom, cornflowerblue, lightsteelblue);">
+
+    <!-- CABECERA -->
   	<div class="row" id="cabecera" style="justify-content: center; align-items: center; background: linear-gradient(to bottom, cornflowerblue, powderblue); /*background-color: cornflowerblue; */height: 100px;">
-    <h1>Administración de Beneficiarios del Área Social (ABAS)</h1>	
+      <h1>Administración de Beneficiarios del Área Social (ABAS)</h1>	
+    
     </div>  
 
-  	<div class="row mt-4" id="leyenda" style="justify-content: center; align-items: center;
-      ">
+    <div class="row mt-4 mb-4 g-4" style="justify-content: center; align-items: center;"> <!-- Div Cuadro -->
+    <div class="col col-sm-3" style="border-radius: 10px; background: linear-gradient(to bottom, royalblue, lightblue);">
+
+    <!-- LEYENDA -->
+  	<div class="row mt-4" id="leyenda" style="justify-content: center; align-items: center;">
   	
-  	<div class="col-7" style="background-color: aqua; border-radius: 10px; font-size: 20px;">
-  		Bienvenido a ABAS, el sistema de Administración de Beneficiarios del Área Social.
-  	</div>
+    	<div class="col-7" style="background-color: aqua; border-radius: 10px; font-size: 20px;">
+    		Bienvenido a ABAS, el sistema de Administración de Beneficiarios del Área Social.
+    	</div>
     
     </div>
 
+
+    <!-- CAJA LOGIN -->
     <div class="row mt-4" id="caja_login" style="justify-content: center; align-items: center;">
 
-  	<div class="col-6">  
+  	 <div class="col col-sm">  
 
-      
-  	<form method="post" action="index_fin.php">  
+
+    <!-- FORMULARIO -->  
   	
-    <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Usuario</label>
-    <input type="text" class="form-control" name="cajaUsuario" aria-describedby="emailHelp" required>
-    <!-- <p id="emailHelp">El usuario debe tener tal cosa.</p> -->
-	</div>
+        <form method="post" action="index_fin.php">  
+  	
+          <div class="mb-3">
+        
+            <label for="exampleInputEmail1" class="form-label">Usuario</label>
+            <input type="text" class="form-control" name="cajaUsuario" aria-describedby="emailHelp" required>
+            <!-- <p id="emailHelp">El usuario debe tener tal cosa.</p> -->
+	        </div>
   
-	<div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label">Contraseña</label>
-    <input type="password" class="form-control" name="cajaPassword" required>
-  	</div>
+	        <div class="mb-3">
+    
+            <label for="exampleInputPassword1" class="form-label">Contraseña</label>
+            <input type="password" class="form-control" name="cajaPassword" required>
+  	     
+          </div>
  	
-    <!-- <input type="submit" value="Buscar"> -->
-  	<button type="submit" class="btn btn-primary">Iniciar sesión</button>
+          <!-- <input type="submit" value="Buscar"> -->
+  	      <button type="submit" class="btn btn-primary mb-3">Iniciar sesión</button>
 
-	</form>
-	</div>
+	      </form>
+	
+      </div>
 
-	</div>
-  	</div>
+	  </div>
+  
+  </div>
+  </div> <!-- Fin Div Cuadro -->
+
+  </div>
     
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
